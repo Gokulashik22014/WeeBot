8335a4b255c68990a9df769fc4e8875afd70e950
Gokulashik M <goku22014.cd@rmkec.ac.in>
2024-07-22 21:11:11 +0530

    updated

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const TabBar = () => {
+const Index = () => {
   return (
     <View>
-      <Text>TabBar</Text>
+      <Text>index</Text>
     </View>
   )
 }
 
-export default TabBar
+export default Index
