8d3bd9a127fac299d17f584b69df1546e07112af
Gokulashik M <goku22014.cd@rmkec.ac.in>
2024-07-25 20:10:55 +0530

    completed

--- app/chat.tsx
+++ app/chat.tsx
@@ -1,7 +1,8 @@
-import { View, Text, SafeAreaView, TextInput } from 'react-native'
-import React, { useCallback, useEffect, useState } from 'react'
-import { GiftedChat, IMessage } from 'react-native-gifted-chat'
-import { useCharacterContext } from '@/provider/CharacterProvider';
+import { View, Text, SafeAreaView, TextInput } from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
+import { GiftedChat, IMessage } from "react-native-gifted-chat";
+import { useCharacterContext } from "@/provider/CharacterProvider";
+import axios from "axios";
 type User = {
   _id: number;
   name: string;
@@ -15,38 +16,75 @@ type Message = {
   user: User;
 };
 const chat = () => {
-  const [messages,setMessages]=useState<IMessage[]>([])
-  const {character}=useCharacterContext()
+  const [messages, setMessages] = useState<IMessage[]>([]);
+  const { character } = useCharacterContext();
+  const [loading,setLoading]=useState(false)
   useEffect(() => {
     setMessages([
       {
         _id: 1,
-        text: 'Hello developer',
+        text: "Hello I am " + character.fullname,
         createdAt: new Date(),
         user: {
           _id: 2,
-          name: 'React Native',
+          name: "React Native",
           avatar: character.dp,
         },
       },
-    ])
-  }, [])
-  const onSend = useCallback((messages = []) => {
-    setMessages(previousMessages =>
-      GiftedChat.append(previousMessages, messages),
-    )
-  }, [])
-  return (
-    <SafeAreaView className='h-full bg-background'>
-      <GiftedChat
-      messages={messages}
-      onSend={messages => onSend(messages)}
-      user={{
-        _id: 1,
-      }}
-    />
-    </SafeAreaView>
-  )
-}
+    ]);
+  }, []);
+  const getResponse = async (message) => {
+    try {
+      setLoading(true)
+      const result = await axios.post("http://192.168.9.28:3000/api/chat", {
+        message: message,
+        desc: character.desc,
+      });
+      if (result.data.success) {
+        const chatResponse = {
+          _id: Math.random() * 999,
+          text: result.data.message,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: character.dp,
+          },
+        };
+        setMessages((previousMessages) =>
+          GiftedChat.append(previousMessages, chatResponse)
+        );
+      }
+      setLoading(false)
+      console.log(result.data);
+    } catch (error) {
+      console.log(error);
+    }finally{
+      setLoading(false)
+    }
+  };
 
-export default chat
+  //handele send function
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+    if (messages[0].text) {
+      getResponse(messages[0].text);
+    }
+  }, []);
+  return (
+    <SafeAreaView className="h-full bg-background">
+      <GiftedChat
+        messages={messages}
+        isTyping={loading}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default chat;
--- constants/characters.ts
+++ constants/characters.ts
@@ -1,37 +1,37 @@
-import image from "./images"
-const data=[
-    {
-        name:"naruto",
-        fullname:"Naruto Uzumaki",
-        desc:"",
-        logo:image.narutoP,
-        dp:image.narutoDP,
-        primary:"#FF8277",
-    },
-    {
-        name:"luffy",
-        fullname:"Monkey D Luffy",
-        desc:"",
-        logo:image.luffyP,
-        dp:image.luffyDP,
-        primary:"#FF0000",
-    },
-    {
-        name:"miku",
-        fullname:"Miku Nakano",
-        desc:"",
-        logo:image.mikuP,
-        dp:image.mikuDP,
-        primary:"#F53FCD",
-    },
-    {
-        name:"anya",
-        fullname:"Anya Forger",
-        desc:"",
-        logo:image.anyaP,
-        dp:image.anyaDP,
-        primary:"#2DB1CF",
-    },
-]
+import image from "./images";
+const data = [
+  {
+    name: "naruto",
+    fullname: "Naruto Uzumaki",
+    desc: "Your are a character from Naruto Shippuden and you will be naruto uzumaki in the conversation act like him and response in the way he does you can give up to 20 words.",
+    logo: image.narutoP,
+    dp: image.narutoDP,
+    primary: "#FF8277",
+  },
+  {
+    name: "luffy",
+    fullname: "Monkey D Luffy",
+    desc: "Your are a character from One Piece and you will be Monkey D Luffy in the conversation act like him and response in the way he does you can give up to 20 words.",
+    logo: image.luffyP,
+    dp: image.luffyP,
+    primary: "#FF0000",
+  },
+  {
+    name: "miku",
+    fullname: "Miku Nakano",
+    desc: "Your are a character from The Quintessential Quintuplets and you will be Miku Nakano in the conversation act like her and response in the way she does you can give up to 20 words.",
+    logo: image.mikuP,
+    dp: image.mikuDP,
+    primary: "#F53FCD",
+  },
+  {
+    name: "anya",
+    fullname: "Anya Forger",
+    desc: "Your are a character from Spy X Family and you will be Anya Forger in the conversation act like her and response in the way she does you can give up to 20 words.",
+    logo: image.anyaP,
+    dp: image.anyaDP,
+    primary: "#2DB1CF",
+  },
+];
 
-export default data
+export default data;
